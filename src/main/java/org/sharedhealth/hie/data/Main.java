e62fd24f1ac6c8e9dbea7ed5553a183f2551641f
Nehashri <nehashrl@thoughtworks.com>
2015-05-05 15:47:08 +0530

    Neha | changed output directory. Reverts previous commit.

--- src/main/java/org/sharedhealth/hie/data/Main.java
+++ src/main/java/org/sharedhealth/hie/data/Main.java
@@ -52,7 +52,7 @@ public class Main {
             throw new RuntimeException("Missing argument(s).");
         }
         String inputDir = args[1];
-        String outputDir = String.format("%s/%s", args[2], proj);
+        String outputDir = args[2];
         boolean createConceptEvent = new Boolean(args[3]);
         new OpenMRSConceptClientScript(createConceptEvent).generate(inputDir, outputDir);
     }
@@ -64,7 +64,7 @@ public class Main {
             throw new RuntimeException("Missing argument(s).");
         }
         String env = args[1];
-        String outputDir = args[2];
+        String outputDir = String.format("%s/%s", args[2], proj);
         String inputDir = String.format("%s/%s", "data", env);
 
         HRM = "prod".equals(env) ? HRM_PROD : HRM_TEST;
