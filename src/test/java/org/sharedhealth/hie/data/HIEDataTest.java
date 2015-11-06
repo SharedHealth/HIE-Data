0a6dbf79e85d2ac8de08f4236531efce949341ab
angshu <angshuonline@gmail.com>
2015-11-06 19:38:18 +0530

    3rd batches of data. cleaning up file

--- src/test/java/org/sharedhealth/hie/data/HIEDataTest.java
+++ src/test/java/org/sharedhealth/hie/data/HIEDataTest.java
@@ -58,7 +58,7 @@ public class HIEDataTest {
     @Test
     @Ignore
     public void cleanupDatasheet() throws Exception {
-        InputStream inputStream = new URL("file:///Users/angshus/work/bdshr/sharedhealth/HIE-Data/src/main/resources/data/openmrs-concept/tr/Panel_tests.csv").openStream();
+        InputStream inputStream = new URL("file:///Users/angshus/work/bdshr/sharedhealth/HIE-Data/src/main/resources/data/openmrs-concept/tr/chw_concepts/child_health_template.csv").openStream();
         Writer writer = null;
         InputStreamReader inputStreamReader = null;
         BufferedReader bufferedReader = null;
@@ -66,7 +66,7 @@ public class HIEDataTest {
         try {
             inputStreamReader = new InputStreamReader(inputStream, "us-ascii");
             bufferedReader = new BufferedReader(inputStreamReader);
-            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/Users/angshus/Documents/Panel_tests.csv"), "utf-8"));
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/Users/angshus/Documents/child_health_template.csv"), "utf-8"));
             while ((line = bufferedReader.readLine()) != null) {
                 writer.write(line.replace("/?", "/").concat("\n"));
             }
